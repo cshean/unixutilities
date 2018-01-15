e2db7dea196bf996a52a92933226a3d8c8467595
cshean <cartershean2@gmail.com>
2018-01-15 00:14:49 -0500

    wrote up method, still has bug

--- cmp.cpp
+++ cmp.cpp
@@ -3,6 +3,20 @@
 #include <string>
 #include <vector>
 
+void findAndPrintByte(std::string line1, std::string line2, std::string file1Name, std::string file2Name, int index){
+	if (line1 == "") {
+		std::cout << file1Name << " " << file2Name << " differ at byte 0 on line " << index << std::endl;
+	}
+
+	for (int i = 0; i < line1.size(); ++i){
+		if (line1.at(i) != line2.at(i)){
+			std::cout << file1Name << " " << file2Name << " differ at byte " << i << " on line " << index << std::endl;
+			return;
+		}
+	}
+}
+
+
 //function overloading for when the user does and does not specify a 
 //command line argument
 //------------------------------------------------------------------
@@ -12,8 +26,29 @@ void compareFiles(std::string file1Name, std::string file2Name){
 	if (file1.is_open()) {
 		std::string fileLine;
 		while (std::getline(file1, fileLine)) {
-
 			file1Lines.push_back(fileLine);
+		}
+		file1.close();
+		std::ifstream file2(file2Name);
+
+		if (file2.is_open()) {
+			int index = 0;
+			
+			while (std::getline(file2, fileLine)) {
+				if (index >= file1Lines.size()) {
+					findAndPrintByte("", fileLine, file1Name, file2Name, index);
+					file2.close();
+					return;
+				} else if (fileLine == file1Lines[index]){
+					continue;
+				} else {
+					findAndPrintByte(file1Lines[index], fileLine, file1Name, file2Name, index);
+					file2.close();
+					return;
+				}
+			}
+		} else {
+			std::cout << "cmp: " << file2Name << ": No such file or directory" << std::endl;
 		}
 	} else {
 		std::cout << "cmp: " << file1Name << ": No such file or directory" << std::endl;

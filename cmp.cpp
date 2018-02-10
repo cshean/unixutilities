3624ee8b5ed17a7e3ada40cf14b2a715b90bd608
cshea892 <cshea892@students.bju.edu>
2018-02-10 02:07:52 -0500

    added class to cmp utility, still has logic error

--- cmp.cpp
+++ cmp.cpp
@@ -4,30 +4,53 @@
 #include <vector>
 
 using namespace std;
-//takes two strings, line1 and line2, and prints the first byte where they are different
-void findAndPrintByte(std::string line1, std::string line2, std::string file1Name, std::string file2Name, int index){
-	if (line1 == "") {
-		std::cout << file1Name << " " << file2Name << " differ at byte 0 on line " << index << std::endl;
-	} else if (line1.size() != line2.size()){
-		int shorterLength = line1.size() > line2.size() ? line2.size() : line1.size();
-		std::cout << file1Name << " " << file2Name << " differ at byte " << shorterLength << " on line " << index << std::endl;
-	} else {
 
-		for (int i = 0; i < line1.size(); ++i){
-			if (line1.at(i) != line2.at(i)){
-				std::cout << file1Name << " " << file2Name << " differ at byte " << i << " on line " << index << std::endl;
-				return;
+class Options {
+	vector<std::string> fileNames;
+	bool lOption = false;
+	bool sOption = false;
+
+	public: 
+		Options(int argc, char * argv[]){
+
+			//if there are less than two command line args, print error message
+			if (argc < 3) {
+				std::cout << "invalid number of files" << std::endl;  
+				std::cout << "Try 'cmp --help' for more information" << std::endl;
+			//if the option used was invalid, print error message 
+			} else if ((argc > 3 && argc < 5) && (std::string(argv[1]) != "-l" && std::string(argv[1]) != "-s")) {
+				std::cout << "invalid --ignore-initial value '" << argv[3] << "'" << std::endl; 
+				std::cout << "Try 'cmp --help' for more information" << std::endl;
+			// if the number of files is correct
+			} else {
+				//check for command line options, and set the flag based on whether or not the option is selected
+				fileNames.push_back(std::string(argv[2]));
+				fileNames.push_back(std::string(argv[3]));
+				(std::string(argv[1]) == "-l") ? lOption = true : sOption = true; 
 			}
+		}
+
+		//accessor methods for classes' variables
+		vector<std::string> getFileNames() { return fileNames; }
+		bool getLOption() { return lOption; }
+		bool getSOption() { return sOption; }
+	
+};
+
+void findByte(int index, int numbytes, Options option, string file1Line, string file2Line){
+	for(int i = 0; i < file1Line.size(); ++i){
+		if (file1Line.at(i) != file2Line.at(i)) {
+			cout << option.getFileNames().at(0) << " " << option.getFileNames().at(1) << "differ: byte " << (numbytes + i) << ", line " << index << endl; 
 		}
 	}
 }
 
-
-//function overloading for when the user does and does not specify a 
-//command line argument
-void compareFiles(std::string file1Name, std::string file2Name){
-	std::vector<std::string> file1Lines;
-	std::ifstream file1(file1Name);
+//function that takes an Options argument and checks whether or not the two files
+//contained in that options object's vector of file names are equal
+void compareFiles(Options option){
+	
+	vector<std::string> file1Lines;
+	std::ifstream file1(option.getFileNames().at(0));
 
 	//if the file exists
 	if (file1.is_open()) {
@@ -39,7 +62,7 @@ void compareFiles(std::string file1Name, std::string file2Name){
 		}
 		//close the file and create a new stream for the second file
 		file1.close();
-		std::ifstream file2(file2Name);
+		std::ifstream file2(option.getFileNames().at(1));
 
 		//if opening the file worked
 		if (file2.is_open()) {
@@ -47,52 +70,39 @@ void compareFiles(std::string file1Name, std::string file2Name){
 			int numbytes = 0;
 			//while there is a new line in the file, compare it to the old file
 			while (std::getline(file2, fileLine)) {
-				numbytes+=fileLine.size();
+			
 				//if the second file is longer than the first, print that and close file
 				if (index >= file1Lines.size()) {
-					findAndPrintByte("", fileLine, file1Name, file2Name, index);
+					findByte(index, numbytes, option, "", "");
 					file2.close();
 					return;
 				//ignore if the lines are equal
 				} else if (fileLine == file1Lines.at(index)){
+					numbytes+=fileLine.size();
 					index++;
 					continue;
 				//if the lines are different, use separate method to find the first character that is different
 				} else {
-					findAndPrintByte(file1Lines.at(index), fileLine, file1Name, file2Name, index);
+					findByte(index, numbytes, option, file1Lines.at(index), fileLine);
 					file2.close();
 					return;
 				}
 			}
 		} else {
-			std::cout << "cmp: " << file2Name << ": No such file or directory" << std::endl;
+			std::cout << "cmp: " << option.getFileNames().at(1) << ": No such file or directory" << std::endl;
 		}
 	//if the first file does not exist, print that fact along with the name
 	} else {
-		std::cout << "cmp: " << file1Name << ": No such file or directory" << std::endl;
+		std::cout << "cmp: " << option.getFileNames().at(0) << ": No such file or directory" << std::endl;
 	}
 } 
 
-void compareFiles(std::string file1, std::string file2, std::string option) {
-	std::cout << "option" << std::endl;
-}
 
 
 
 
 int main(int argc, char * argv[]){
-	//if there are less than two command line args, print error message
-	if (argc < 3) {
-		std::cout << "invalid number of files" << std::endl;  
-		std::cout << "Try 'cmp --help' for more information" << std::endl;
-	//if the option used was invalid, print error message 
-	} else if (argc > 3 && std::string(argv[1]) != "-l" && std::string(argv[1]) != "-s") {
-		std::cout << "invalid --ignore-initial value '" << argv[3] << "'" << std::endl; 
-		std::cout << "Try 'cmp --help' for more information" << std::endl;
-	// if the number of files is correct
-	} else {
-		//check for command line options, and use the appropriate logic based on whether or not one exists
-		argc > 3 ? compareFiles(std::string(argv[2]), std::string(argv[3]), std::string(argv[1])) : compareFiles(std::string(argv[1]), std::string(argv[2]));
-	}
+	Options option(argc, argv);
+	compareFiles(option);
 	return 0;
 }
